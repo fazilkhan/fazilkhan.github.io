a5bd30b5e3cc298e05687a63c4e861709a394def
fazilkhan <khanfazil444@gmail.com>
2021-08-12 06:12:02 -0400

    Mapbox Story Upload

---
+++ static/puerto-rico-vacant-houses/config.js
@@ -0,0 +1,352 @@
+let topTitleDiv = "<h4>Storytelling with Data | Migration Group</h4>";
+
+let titleDiv =
+  "<h1 id='topTitle'>Empty and decaying</h1>" +
+  "<h3 id='subheader'>Climate and economic migration leaves thousands of houses abandoned in Puerto Rico</h3>";
+
+let bylineDiv = "<p class='byline'><i>By Kwasi Gyamfi Asiedu, Fazil Khan, Karthika Namboothiri, and Paroma Soni</i></p>";
+
+let descriptionDiv =
+  '<p><span class="firstcharacter">W</span>ading around her home four feet deep in water, struggling to battleempty the flooding one bucket at a time, Paola Hernandez Soto and her family watched helplessly as Hurricane Maria blew the wooden roof off their house.</p>' +
+  '<p>The fierce winds from the category 5 hurricane did the same to Soto’s grandmother’s roof, and several others in the little town of Moca in the northwest of Puerto Rico.</p>' +
+  '<p>“A lot of trees fell and enclosed us. It was so scary, and there was no electricity, no water, no food or fuel, no communication for almost a year,” Soto, 23, said. “It took us eight or nine months to fix the house. And even today, there are so many people still using blue plastic tents instead of proper roofs because they couldn’t rebuild,” she said in an interview four years on.</p>' +
+  '<p>The result was a large exodus of Puerto Ricans from the archipelago to the US mainland. Puerto Ricans have been migrating to the mainland for decades, but in the last few years, the levels of emigration have been significant.</p>' +
+  '<p>The devastation caused by Hurricane Maria exacerbated deep-rooted problems already present on the territory, including a poverty rate four times the national average and a pre-pandemic unemployment rate nearly three times higher than the national figure.</p>' +
+  '<p>The already fragile real estate market was among the most heavily affected. With many unable or unwilling to rebuild their homes, there has been a surge in abandoned, vacant houses across the island followed as people emigrated to restart their lives.</p>' +
+  '<p style="text-align:center">Scroll to continue<br><span class="blinking">▼</span></p>';
+
+let footerDiv =
+  '<p><a href="https://www.mapbox.com/about/maps/" target="_blank">© Mapbox</a> | <a href="http://www.openstreetmap.org/about/" target="_blank">© OpenStreetMap</a> | <a href="https://brown.columbia.edu">The Brown Institute for Media Innovation</a></p>';
+
+let divChapter1 =
+  "<h3><strong>Thousands of Vacant Homes Spread Across the Island</strong></h3>" +
+  '<svg class="sameLine" width="370" height="15" version="1.1" xmlns="ttps://www.w3.org/TR/SVG2/"><style type="text/css">rect{fill:url(#MyGradient)}</style><defs><linearGradient id="MyGradient"><stop offset="30%" stop-color="#fdd0a2" /><stop offset="80%" stop-color="#f16913" /><stop offset="95%" stop-color="#7f2704" /></linearGradient></defs><rect width="370" height="15"/></svg><p class="sameLine">0 - 50,000</p></br>' +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#710e0e" /></svg><p class="sameLine">Flood risk</p>' +
+  '<img src="images/stockchapter1.jpeg">' +
+  '<p class="imageCredit">Representative image</a></p>' +
+  "<p>Puerto Rico had more than 300,000 vacant/abandoned houses as of 2019. This was nearly one-fifth of all housing units on the island. A significant number of these were concentrated in and around San Juan. The San Juan county alone had more than 42,000 vacant homes.</p>";
+  // '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#4292c6" /></svg><p class="sameLine">10,001 - 20,000</p>' +
+  // '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#084594" /></svg><p class="sameLine">20,001 - 50000</p>';
+
+let divChapter2 =
+  "<h3>Destroyed by the Hurricane | Palo Seco</h3>" +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#fdae6b" /></svg><p class="sameLine">Vacant/Abandoned Homes</p>' +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#710e0e" /></svg><p class="sameLine">Flood risk</p>' +
+  '<img src="images/mariaOverPR.gif">' +
+  '<p class="imageCredit">Hurricane Maria over Puerto Rico on September 20, 2017. Courtesy: NOAA</a></p>' +
+  "<p>A lot of Palo Seco’s properties had been abandoned for decades, or were destroyed by Maria. The bridge in the middle of this area was most severely impacted by the hurricane, and the houses there were completely ruined. In perfect condition just a few years prior, these homes now stand vacant.</p>";
+let divChapter3 =
+  "<h3>Everyone's migrating | Levittown</h3>" +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#fdae6b" /></svg><p class="sameLine">Vacant/Abandoned Homes</p>' +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#710e0e" /></svg><p class="sameLine">Flood risk</p>' +
+  "<p>Lack of employment and poverty is another major reason why Puerto Ricans are migrating to the mainland US. Poverty rates on the island have consistently remained higher than the national average.</p>" +
+  '<img src="images/povertyPR.png">' +
+  "<p>However, even people from high-income counties are leaving. Levittown is one such affluent locality that has abandoned homes scattered across the neighborhood as people leave the island.</p>";
+
+let divChapter4 =
+  "<h3>Illegal Structures and Disaster Vulnerability | Valle Hill</h3>" +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#fdae6b" /></svg><p class="sameLine">Vacant/Abandoned Homes</p>' +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#710e0e" /></svg><p class="sameLine">Flood risk</p>' +
+  "<p>Valle Hill lies in a flood plain, meaning it is susceptible to flooding even with small amounts of rain. An area developed only 30 years ago, it has hundreds abandoned properties, some of which are illegal structures and unmanaged trash dumps.</p>";
+
+let divChapter5 =
+  "<h3>An abandoned home at every corner</h3>" +
+  "<p>Even as San Juan accounts for the highest number of vacant/abandoned homes in absolute numbers, nearly all counties have at least 500 such housing units per 10,000 people.</p></br>" +
+  '<svg class="sameLine" width="370" height="15" version="1.1" xmlns="ttps://www.w3.org/TR/SVG2/"><style type="text/css">rect{fill:url(#MyGradient)}</style><defs><linearGradient id="MyGradient"><stop offset="16%" stop-color="#fdd0a2" /><stop offset="33%" stop-color="#fdae6b" /><stop offset="50%" stop-color="#fd8d3c" /><stop offset="66%" stop-color="#f16913" /><stop offset="83%" stop-color="#d94801" /><stop offset="96%" stop-color="#8c2d04" /></linearGradient></defs><rect width="370" height="15"/></svg><p class="sameLine">0 - 3,000+</p></br>' +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#710e0e" /></svg><p class="sameLine">Flood risk</p>' +
+  '<img src="images/histogram-01.png">';
+  
+  let divChapter6 =
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>According to the 2020 census, Puerto Rico’s population has declined by <a href='https://www.census.gov/quickfacts/PR' target='_blank'>13 percent</a> compared to the 2010 census. In 2018, a year after the Hurricane, Puerto Rico’s net migration rate, the difference between the number of immigrants entering and leaving, had <a href='https://www.macrotrends.net/countries/PRI/puerto-rico/net-migration' target='_blank'>tripled</a> to a record 31 compared to a rate of about 10 in 2010.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“The post Hurricane Maria exodus represents one of the most significant movements of Puerto Ricans to the U.S. mainland in the island’s history in terms of both volume and duration,” a 2020 report by researchers, at the Center for Puerto Rican Studies, City University of New York, said.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“It wasn't just working-age people or students leaving. It was grandparents leaving, entire families leaving – because they were unable to stay in their homes and things were just getting worse,” said Jennifer Hinojosa, data coordinator at the center.</p>" +
+  // '<div style="max-width:620px; margin-left:auto; margin-right:auto"><img src="images/unemployment.png"></div>' +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“I cried a lot because my sister left, my little cousins left and my best friend left too,” Ms. Soto recalled. “It was really sad because I was like ‘everyone is leaving me, just stay, we can do it.”</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Puerto Rico’s vacancy rate of <a href='https://censusreporter.org/data/map/?table=B25002&geo_ids=04000US72,01000US,04000US36,04000US12,04000US06,04000US17&primary_geo_id=04000US72#column%7CB25002003,sumlev%7C040' target='_blank'>25 percent</a> is twice the national average, according to the 2019 American Community Survey, the latest available data. “In some neighborhoods, entire blocks are abandoned, said Luis Gallardo Rivera, founder of the Center for Habitat Reconstruction, a San Juan-based non-profit that converts such properties into affordable housing and community projects.</p>" +
+  '<div style="max-width:620px; margin-left:auto; margin-right:auto"><img src="images/vacancyRate.png"></div>' +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“They destroy the local housing market. It brings crime, rodents, trash accumulation, vandalism. These are public security issues that lead to the tax base dropping and property values being impacted,” said Rivera, explaining that the current complex inheritance laws make it harder to address the problem.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“Studies also show that folks who live near abandoned properties are more receptive to infectious diseases. It really impacts the community psychologically, negatively,” Rivera said.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>The organization works in specific neighborhoods that have high numbers of vacant and abandoned homes, including Palo Seco and Levittown in the coastal capital, San Juan, and Valle Hill just south of it.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Because San Juan is the most populous city with more than 330,000 residents, it saw the greatest exodus in the wake of Maria. However, abandoned homes are present across the island.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>As a fresh hurricane season approaches in a year that has already seen unprecedented extreme weather events, the preparedness of Puerto Rico to withstand another climate catastrophe is still unclear.</p>";
+
+  let divChapter7 =
+  "<h3>Future Risks of Destruction from Natuaral Disasters Still Remain</h3>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Many regions in Puerto Rico continue to face massive risks of flooding, storm surges, and wind damage from extreme climate events. The National Oceanic and Atmospheric Association which identifies the zones most at risk from each hurricane category, recently updated with data from Hurricane Maria. Inevitably the properties and infrastructure in these zones are most at risk in the event of a similar superstorm, and in San Juan specifically, they may well lead to a new set of abandoned homes if people aren’t as well prepared.</p>" +
+  '<svg class="sameLine" height="18" width="18"><circle cx="9" cy="9" r="7" fill="#710e0e" /></svg><p class="sameLine">Flood risk</p>';
+
+  let divChapter8 =
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“When Maria happened, there was no town that was not touched, because Puerto Ricans didn’t prepare as they should have,” said Soto.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>Four years on, her sister, a teacher, and her husband are settled in Texas where they are expecting a baby. Despite feeling the toll of familial and friendly separation, Soto, like many other Puerto Ricans, is not giving up on her beloved home and has hopes for the future.</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“Right now, I am not thinking about leaving,” she said. “I really love Puerto Rico, I think everything can get better over the years, and it will just take time.”</p>" +
+  "<p style='max-width:600px; margin-left:auto; margin-right:auto'>“The people are funny, they are happy, there are people with good hearts, the sun, the beaches, everything is beautiful,” she said.</p>";
+
+var config = {
+  style: "mapbox://styles/fazilkhan/ckrp0sqgiby1917ok8vqwm8g9",
+  accessToken:
+    "pk.eyJ1IjoiZmF6aWxraGFuIiwiYSI6ImNrcm04MzZwdjB5MmIyb285NWF0azFnazcifQ.oMhbhiUQYSE4pSXXajZX-A",
+  showMarkers: false,
+  markerColor: "#3FB1CE",
+  theme: "light",
+  use3dTerrain: false,
+  topTitle: topTitleDiv,
+  title: titleDiv,
+  subtitle: "",
+  byline: bylineDiv,
+  description: descriptionDiv,
+  footer: footerDiv,
+  chapters: [
+    {
+      id: "overallMap",
+      alignment: "left",
+      hidden: false,
+      chapterDiv: divChapter1,
+      location: {
+        center: [-67, 18.23],
+        zoom: 8.25,
+        zoomSmall: 7,
+        pitch: 40,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "VacHousePoints",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "VacHousePoints",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "paloSaco",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter2,
+      location: {
+        center: [-66.14370237519364,18.461972504530394],
+        zoom: 16,
+        zoomSmall: 16,
+        pitch: 40,
+        bearing: -7,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "counties_outline",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "counties_outlines",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "levittown",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter3,
+      location: {
+        center: [-66.17309402697919,18.44806080407271],
+        zoom: 16.5,
+        zoomSmall: 16,
+        pitch: 40,
+        bearing: 8,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "counties_outline",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "counties_outline",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "vallehill",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter4,
+      location: {
+        center: [-65.89441621121745,18.397010344701172],
+        zoom: 16.5,
+        zoomSmall: 14,
+        pitch: 40,
+        bearing: -7,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "counties_outline",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "counties_outline",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "VacHouseOverlay",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter5,
+      location: {
+        center: [-67, 18.23],
+        zoom: 8.25,
+        zoomSmall: 7,
+        pitch: 30,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "vacantHouses",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "vacantHouses",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "VacHouseOverlay",
+      alignment: "full",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter6,
+      location: {
+        center: [-67, 18.23],
+        zoom: 8.25,
+        zoomSmall: 7,
+        pitch: 30,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "vacantHouses",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "vacantHouses",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "radar-layer",
+      alignment: "left",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter7,
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "radar-layer",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "radar-layer",
+          opacity: 1,
+          duration: 300,
+        },
+      ],
+    },
+    {
+      id: "VacHouseOverlay",
+      alignment: "center",
+      hidden: false,
+      title: "",
+      image: "",
+      description: "",
+      chapterDiv: divChapter8,
+      location: {
+        center: [-67, 18.23],
+        zoom: 8.25,
+        zoomSmall: 7,
+        pitch: 30,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "vacantHouses",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "vacantHouses",
+          opacity: 0,
+          duration: 300,
+        },
+      ],
+    },
+  ],
+};
